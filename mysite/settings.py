f39a7f8b604f335c0adf7991d3acdba97eff1d5c
root <g.cole.killian@gmail.com>
2019-09-27 02:39:06 +0000

    change debug to false

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,10 +25,10 @@ with open('/etc/colekillian.com-secret_key.txt') as f:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
-ALLOWED_HOSTS = ['157.245.140.109', 'colekillian.com', 'localhost', '142.157.10.81']
+ALLOWED_HOSTS = ['157.245.140.109', 'colekillian.com', 'localhost']
 
 
 INSTALLED_APPS = [
